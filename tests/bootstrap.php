87fec47d2e8de630b945c1fa998fe942935e96a4
valentin v / vvval <valentin@spiralscout.com>
2019-02-20 11:47:18 +0300

    bootstrap with strict types

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * Spiral Framework, SpiralScout LLC.
@@ -7,7 +8,7 @@
  */
 
 error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', true);
+ini_set('display_errors', '1');
 
 //Composer
 require dirname(__DIR__) . '/vendor/autoload.php';
