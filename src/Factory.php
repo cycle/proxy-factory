aedaa6a493b7c3a2207ac021c78edf130be5817b
valentin v / vvval <valentin@spiralscout.com>
2019-03-07 15:59:48 +0300

    add deps

--- src/Factory.php
+++ src/Factory.php
@@ -5,32 +5,26 @@ namespace Cycle\ORM\Promise;
 
 use Cycle\ORM\ORMInterface;
 use Cycle\ORM\Promise\Declaration\Declaration;
-use Cycle\ORM\Promise\Materizalizer\EvalMaterializer;
 use Cycle\ORM\Promise\Naming\DatetimeNaming;
 use Cycle\ORM\PromiseFactoryInterface;
 use Cycle\ORM\Schema;
-use Spiral\Core\Container;
 
 class Factory implements PromiseFactoryInterface
 {
-    /** @var Container */
-    private $container;
-
     /** @var ProxyPrinter */
     private $creator;
-
-    /** @var NamingInterface */
-    private $naming;
 
     /** @var MaterializerInterface */
     private $materializer;
 
-    public function __construct(Container $container, ProxyPrinter $creator, ?NamingInterface $naming, MaterializerInterface $materializer)
+    /** @var NamingInterface */
+    private $naming;
+
+    public function __construct(ProxyPrinter $creator, MaterializerInterface $materializer, ?NamingInterface $naming)
     {
-        $this->container = $container;
         $this->creator = $creator;
+        $this->materializer = $materializer;
         $this->naming = $naming ?? new DatetimeNaming();
-        $this->materializer = $materializer ?? new EvalMaterializer();
     }
 
     public function promise(ORMInterface $orm, string $role, array $scope): ?ReferenceInterface
@@ -45,11 +39,11 @@ class Factory implements PromiseFactoryInterface
 
         $this->materializer->materialize($output, $declaration);
 
-        return $this->makeProxyObject($orm, $role, $scope, $declaration->class->getNamespacesName());
+        return $this->instantiate($orm, $role, $scope, $declaration->class->getNamespacesName());
     }
 
-    private function makeProxyObject(ORMInterface $orm, string $role, array $scope, string $className)
+    private function instantiate(ORMInterface $orm, string $role, array $scope, string $className): PromiseInterface
     {
-        return $this->container->make($className, compact('orm', 'role', 'scope'));
+        return new $className($orm, $role, $scope);
     }
 }
