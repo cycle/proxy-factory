84de6340c5483dc476310f73ec13850c15a2c8e1
valentin v / vvval <valentin@spiralscout.com>
2019-04-15 12:03:32 +0300

    Remove mockery

--- tests/Promise/ProxyPrinterTest.php
+++ tests/Promise/ProxyPrinterTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Cycle\ORM\Promise\Tests;
 
+use Cycle\Annotated\Entities;
 use Cycle\ORM\ORMInterface;
 use Cycle\ORM\Promise\Declaration\Declarations;
 use Cycle\ORM\Promise\Declaration\Extractor;
@@ -12,13 +13,42 @@ use Cycle\ORM\Promise\PromiseResolver;
 use Cycle\ORM\Promise\ProxyPrinter;
 use Cycle\ORM\Promise\Tests\Fixtures;
 use Cycle\ORM\Promise\Utils;
+use Cycle\Schema;
 use PhpParser\PrettyPrinter\Standard;
 use PhpParser\PrettyPrinterAbstract;
-use PHPUnit\Framework\TestCase;
 use Spiral\Core\Container;
+use Spiral\Database\Driver\SQLite\SQLiteDriver;
 
-class ProxyPrinterTest extends TestCase
+class ProxyPrinterTest extends BaseTest
 {
+    public const DRIVER = 'sqlite';
+
+    /** @var \Spiral\Core\Container */
+    private $container;
+
+    public function setUp()
+    {
+        self::$config = [
+            'debug'     => false,
+            'strict'    => true,
+            'benchmark' => false,
+            'sqlite'    => [
+                'driver' => SQLiteDriver::class,
+                'check'  => function () {
+                    return !in_array('sqlite', \PDO::getAvailableDrivers());
+                },
+                'conn'   => 'sqlite::memory:',
+                'user'   => 'sqlite',
+                'pass'   => ''
+            ],
+        ];
+
+        parent::setUp();
+
+        $this->container = new Container();
+        $this->container->bind(ORMInterface::class, $this->orm());
+    }
+
     public function testDeclaration(): void
     {
         $class = Fixtures\Entity::class;
@@ -432,12 +462,7 @@ class ProxyPrinterTest extends TestCase
      */
     private function makeProxyObject(string $className, string $proxyFullName)
     {
-        $orm = \Mockery::mock(ORMInterface::class);
-
-        $container = new Container();
-        $container->bind(ORMInterface::class, $orm);
-
-        return $container->make($proxyFullName, ['role' => $className, 'scope' => []]);
+        return $this->container->make($proxyFullName, ['role' => $className, 'scope' => []]);
     }
 
     private function make(\ReflectionClass $reflection, Declarations $declaration): string
@@ -451,5 +476,21 @@ class ProxyPrinterTest extends TestCase
         $container->bind(PrettyPrinterAbstract::class, Standard::class);
 
         return $container->get(ProxyPrinter::class);
+    }
+
+    private function orm(): ORMInterface
+    {
+        $schema = (new Schema\Compiler())->compile(new Schema\Registry($this->dbal), [
+            new Entities($this->locator),
+            new Schema\Generator\ResetTables(),
+            new Schema\Generator\GenerateRelations(),
+            new Schema\Generator\ValidateEntities(),
+            new Schema\Generator\RenderTables(),
+            new Schema\Generator\RenderRelations(),
+            new Schema\Generator\SyncTables(),
+            new Schema\Generator\GenerateTypecast(),
+        ]);
+
+        return $this->withSchema(new \Cycle\ORM\Schema($schema));
     }
 }
